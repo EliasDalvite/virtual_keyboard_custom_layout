4a9b5313f3086efebe021adb2aa564d94d5ba8ac
Elias Dalvite <dalvite.elias@gmail.com>
2023-03-02 16:41:40 -0300

    Revert "Functional Code"
    This reverts commit d923f6c4fb5ad5e0d48da48f26d94812d424c8de.

--- lib/src/layouts.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-part of virtual_keyboard_custom_layout;
-
-enum VirtualKeyboardDefaultLayouts { Arabic, English, Custom }
--- lib/src/type.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-part of virtual_keyboard_custom_layout;
-
-/// Available Virtual Keyboard Types:
-/// `Numeric` - Numeric only.
-/// `Alphanumeric` - Alphanumeric: letters`[A-Z]` + numbers`[0-9]` + `@` + `.`.
-/// `Custom` - Customized matrix passed by parameter.
-enum VirtualKeyboardType { Numeric, Alphanumeric, Custom }
